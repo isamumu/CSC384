20ca2057e2d402132b0ccb7be2a743db6f7b0ab0
Isamu <isamu.poy@mail.utoronto.ca>
2021-02-10 12:45:18 +0900

    autograder

--- lab1/solution.py
+++ lab1/solution.py
@@ -146,7 +146,7 @@ def heur_alternate(state):
     
     mini = min(cost)
     # mini2 = min(cost2)
-    return mini #+ heur_manhattan_distance(state) # + mini2
+    return heur_manhattan_distance(state) # + mini2
 
 def heur_zero(state):
     '''Zero Heuristic can be used to make A* search perform uniform cost search'''
